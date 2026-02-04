a380498e9ada40900eb9635da90d5dd60e91d21e
Promise Siafwiyo <primevaldigitals@gmail.com>
2026-02-04 06:39:01 +0200

    made a navbar

--- src/page1.jsx
+++ src/page1.jsx
@@ -6,7 +6,35 @@ function Page1() {
 
     return(
         <>
-            <h1>Hie my name is Promise</h1>
+            <div id='page1'>
+
+                <nav>
+                    <section><img src="/primegotit logo blue big.png" alt="" /><h3>Primegotit</h3></section>
+
+                    <ul>
+                        <li><a href="#" className='hover-link'>Home</a></li>
+                        <li><a href="#" className='hover-link'>Services</a></li>
+                        <li><a href="#" className='hover-link'>About me</a></li>
+                        <li><a href="#" className='hover-link'>Contact me</a></li>
+
+                    </ul>
+
+                    <section><img  id='more-icon' src="/menu burger-icon.png" alt="" /></section>
+                </nav>
+
+                <main>
+
+                    <div>
+
+                    </div>
+                    
+
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </main>
+
+            </div>
         </>
     )
 
