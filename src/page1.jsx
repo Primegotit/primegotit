d1d77b021e832aae07a3369f844408d4b85ae141
Promise Siafwiyo <primevaldigitals@gmail.com>
2026-02-04 05:50:00 +0200

    added some new files

---
+++ src/page1.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './page1.css'
+
+
+function Page1() {
+
+    return(
+        <>
+            <h1>Hie my name is Promise</h1>
+        </>
+    )
+
+}
+
+export default Page1;
