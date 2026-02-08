1a7c60506cb268cf475581538e17c43e2a1cec30
Promise Siafwiyo <primevaldigitals@gmail.com>
2026-02-08 06:22:11 +0200

    fixing the desktop laptop

--- src/page1.jsx
+++ src/page1.jsx
@@ -23,41 +23,43 @@ function Page1() {
                     <section><img  id='more-icon' src="/menu burger-icon.png" alt="" /></section>
                 </nav>
 
-                <main>
+                <div id='main'>
 
-                    <div id='my-content'>
-                        <h1 id='intro-text'>Who Am I ?</h1>
-                        <h2>Promise Siafwiyo</h2>
-                        <p>There are a lot of words i can use to describe myself but all i will say is I am an <b>Interdisciplinary specialist.</b> Join me in my journey to becoming a better technophile, here you can view all of my projects. </p>
-                        <section id='skills-container'>
-                            <a href="#" className='skills'>What am I ?</a>
-                            <a href="#" className='skills'>Music producer</a>
-                            <a href="#" className='skills'>Animator</a>
-                            <a href="#" className='skills'>Graphic designer</a>
-                            <a href="#" className='skills'>Private tutor</a>
-                            <a href="#" className='skills'>Video editor</a>
-                            <a href="#" className='skills'>Aspiring hacker</a>
-                            <a href="#" className='skills'>Programmer</a>
-                            {/* <a href="#" className='skills'>Anatagonist</a> */}
-                            <a href="#" className='skills'>Greyscale lover</a>
-                            
-                            
-                        </section>
+                        <div id='my-content'>
 
-                        <section >
-                            <a href="#" id='view-work-btn' >View my work <TiArrowRightThick style={{color: 'white', marginLeft: '10px', width: '35px', height: '35px'}}/></a>
-                        </section>
+                            <section id='content-box'>
+                                <h1 id='intro-text'>Who Am I ?</h1>
+                                <h2>Promise Siafwiyo</h2>
+                                <p>There are a lot of words i can use to describe myself but all i will say is I am an <b>Interdisciplinary specialist.</b> Join me in my journey to becoming a better technophile, here you can view all of my projects. </p>
+                                <section id='skills-container'>
+                                    <a href="#" className='skills'>What am I ?</a>
+                                    <a href="#" className='skills'>Music producer</a>
+                                    <a href="#" className='skills'>Animator</a>
+                                    <a href="#" className='skills'>Graphic designer</a>
+                                    <a href="#" className='skills'>Private tutor</a>
+                                    <a href="#" className='skills'>Video editor</a>
+                                    <a href="#" className='skills'>Aspiring hacker</a>
+                                    <a href="#" className='skills'>Programmer</a>
+                                    {/* <a href="#" className='skills'>Anatagonist</a> */}
+                                    <a href="#" className='skills'>Greyscale lover</a>
+                                    
+                                    
+                                </section>
 
+                                <section >
+                                    <a href="#" id='view-work-btn' >View my work <TiArrowRightThick style={{color: 'white', marginLeft: '10px', width: '35px', height: '35px'}}/></a>
+                                </section>
 
-                    </div>
-                    
+                            </section>
+                        </div>
+                        
 
-                    <div id='my-picture'>
-                        <img id='me' src="promise big image 2.png" alt="" />
-                        {/* <video muted loop autoPlay playsInline></video> */}
+                        <div id='my-picture'>
+                            <img id='me' src="promise big image 2.png" alt="" />
+                            {/* <video muted loop autoPlay playsInline></video> */}
 
-                    </div>
-                </main>
+                        </div>
+                </div>
 
 
 
