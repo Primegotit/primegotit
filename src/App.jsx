29312c35fdefd1618442d83c379e4da952e9fb78
Promise Siafwiyo <primevaldigitals@gmail.com>
2026-02-12 05:27:31 +0200

    adding everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Page1 from './page1'
+import Page2 from './page2'
 
 function App() {
 
   return (
     <>
       <Page1/>
+      
+      <Page2/>
     </>
   )
 }
--- src/page2.jsx
+++ src/page2.jsx
@@ -1,12 +1,15 @@
 import './page2.css'
 
-function page2() {
+function Page2() {
 
     return(
         <>
             <div id='page2'>
-
+                <h3>Hie my name is Promise and these are the services that i offer</h3>
+                <a href="">download</a>
             </div>
         </>
     )
 }
+
+export default Page2;
