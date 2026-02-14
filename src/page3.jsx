7ed1cfa4e302106510218c0ba06d8d05ec77ef6f
Promise Siafwiyo <primevaldigitals@gmail.com>
2026-02-14 07:31:03 +0200

    st

--- src/page3.jsx
+++ src/page3.jsx
@@ -1,4 +1,6 @@
 import './page3.css'
+import { TiArrowRightThick } from "react-icons/ti";
+
 
 function Page3(){
 
@@ -6,29 +8,43 @@ function Page3(){
         <>
             <div id='page3'> 
                 {/* <h1>Graphic design... 💀</h1> */}
-                <div id='big-text-container'>
-                    <h1>I Design.</h1>
-                    <h1>I Create.</h1>
-                    <h1>I Remake.</h1>
-                    <h6>💀</h6>
+                <div id='text-con'>
+                    <div id='all-text-container'>
+                        <div id='big-text-container'>
+                            <h1>I Design.</h1>
+                            <h1>I Create.</h1>
+                            <h1>I Remake.</h1>
+                            {/* <h6>💀</h6> */}
 
+                        </div>
+
+                        <div id='small-text-container'>
+                            <h6>Come on and check out my designs. I let my art express myself. I wish to motivate you so join me on this journey.</h6>
+                        </div>
+
+                
+                    </div>
+
+                    <div id='logo-container'>
+                        {/* <img src="/primegotit logo green big 2.png" alt=""  /> */}
+                        <img src="/Untitdfsdled-1.jpg" alt=""  />
+
+                    </div>
                 </div>
-
-                <div id='small-text-container'>
-                    <h6>Come on and check out my designs. I let my art express myself. I wish to motivate you so join me on this journey.</h6>
-                </div>
-
+                
+                
                 <div id='tech-software'>
                     <ul>
-                        <li><a href="#">Adobe Photoshop</a></li>
-                        <li><a href="#">Blender</a></li>
-                        <li><a href="#">Adobe Lightroom</a></li>
-                        <li><a href="#">Adobe Photoshop</a></li>
-                        <li><a href="#">Blender</a></li>
-                        <li><a href="#">Adobe Lightroom</a></li>
-                        <li><a href="#">Adobe Photoshop</a></li>
-                        <li><a href="#">Blender</a></li>
-                        <li><a href="#">Adobe Lightroom</a></li>
+                        <li><a href="#">Adobe Photoshop  <TiArrowRightThick/> </a></li>
+                        <li><a href="#">Blender <TiArrowRightThick/></a></li>
+                        <li><a href="#">Adobe Lightroom <TiArrowRightThick/></a></li>
+                        <li><a href="#">Adobe Illustrator <TiArrowRightThick/></a></li>
+                        <li><a href="#">Pinterest <TiArrowRightThick/></a></li>
+                        <li><a href="#">Moho <TiArrowRightThick/></a></li>
+                        <li><a href="#">PicsArt <TiArrowRightThick/></a></li>
+                        <li><a href="#">Adobe Animator <TiArrowRightThick/></a></li>
+                        <li><a href="#">Dark Aesthetic <TiArrowRightThick/></a></li>
+
 
                     </ul>
                 </div>
