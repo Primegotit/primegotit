dadce98c598e27db9e8ebc58956ac2e12ceeb381
Promise Siafwiyo <primevaldigitals@gmail.com>
2026-02-21 08:26:15 +0200

    added another cool line

--- src/page4.jsx
+++ src/page4.jsx
@@ -20,58 +20,53 @@ function Page4() {
                 <div id='about-me-container'>
                     <div id='about-me-box'>
                         <div id='about-logo-container'>
-                            <img src="/primegotit logo green big 2.png" id='text-arrow' alt="" />
+                            <img src="/primegotit logo green big 2.png" id='text-arrow' />
                             <div id='logo-line'></div>
                         </div>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                             Fugit totam amet cumque accusamus unde nostrum rem
-                              tempore alias, ipsam blanditiis. Lorem ipsum dolor sit amet 
-                              consectetur, adipisicing elit. Vero fugiat qui deleniti quod 
-                              corporis odio, explicabo, pariatur perspiciatis enim asperiores
-                               aut quae repellat quibusdam rem consectetur inventore iure,
 
-                                magni accusamus.
+
+                        <div id='me-text-container'>
+                            <div id='me-text'>
+                                <p><b>Hello</b>, my name is <b>Promise Siafwiyo</b> also known as <b>Prime</b> and I go by the music 
+                        producer tag "<i><b>PrimeGotit</b></i>". I am a multidisciplinary digital creator, something of a jack all trades and perhaps a master of one or two.
 
                         </p>
                             {/* <CgArrowsExchangeAltV  id='text-arrow'></CgArrowsExchangeAltV> */}
                         <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Labore suscipit ducimus voluptas officiis nulla non debitis,
-                             at odit laboriosam doloremque laborum iure aperiam expedita vel
-                              fuga deleniti! Repellendus, iusto. Praesentium mollitia 
-                              itaque nulla sit rem deleniti possimus eveniet aut, voluptatibus officiis
-                               veritatis tempora modi non illum eos ad corrupti alias. 
-                            perferendis qui! Ex ipsam explicabo necessitatibus expedita iusto nobis, sed odio esse! Sint, vitae tenetur!
-                            
+                            I am currently pursuing a <b>Bachelor's Degree in Computer Science</b> at the <b>National University of Science and Technology in Zimbabwe.</b>
+                            I have always had a passion for computers and technology and that interest has continued to grow over the years.
+                            I'm also a junior developer and animator at <b><a href="www.rodneytech.co.zw">Rodneytech,inc</a></b>, a technology company where i contribute 
+                            to both development and creative projects like animation and etc.
+                            Since 2024, I have also been a private <b>computer acience tutor</b>, mentoring students in programming fundamentals, networking, algorithms and 
+                            data structures, operating system fundamentals and other related modules. Through tutoring
+                            this experience has strengthened my communication skills, deepened my mastery of core principles and greatly improved my confidence.
+
                         </p>    
                             {/* <CgArrowsExchangeAltV  id='text-arrow'></CgArrowsExchangeAltV> */}
 
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                             Fugit totam amet cumque accusamus unde nostrum rem
-                              tempore alias, ipsam blanditiis. Lorem ipsum dolor sit amet 
-                              consectetur, adipisicing elit. Vero fugiat qui deleniti quod 
-                              corporis odio, explicabo, pariatur perspiciatis enim asperiores
-                               aut quae repellat quibusdam rem consectetur inventore iure,
-
-                                magni accusamus.
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                Sunt cum, enim sapiente sint quia harum excepturi iure 
-                                blanditiis in quisquam ab voluptatum exercitationem, vel 
-                                tenetur, laborum aliquid ipsam? Similique obcaecati ducimus, 
-                                facere dolore quaerat reiciendis soluta numquam magnam quos 
-                                dolorem delectus quidem illum, excepturi alias illo modi quasi
-                                 perferendis fuga cupiditate natus eos expedita accusamus 
-                                 nesciunt quis! Doloribus aspernatur praesentium voluptas 
-                                 quam nemo soluta, blanditiis excepturi iusto eaque obcaecati
-                                  optio mollitia. Id ab doloribus voluptatum ex dicta
-                                 voluptatibus alias officiis magni, ad molestias, delectus error suscipit. Facilis quod ea veniam!
-
+                        <p>My journey into the digital field began with music production, where I first explored creativity
+                            through sound design and composition. Over time, that creative curiosity expanded into programming during high school while I was 
+                            studying Computer Science. As I advanced, I realized that building meaningful digital experiences requires more than writing code, it demands 
+                            an understanding of design, structure, user experience and visual communication. Exploring these interconnected disciplines has shaped my approach to technology, combining 
+                            logic, creativity and precision to produce well rounded digital work.<br/>I enjoy creating content across all the disciplines I work, so this website will serve as a complete showcase of my projects, 
+                            carefully organized for easier navigation and viewing.
                         </p>
+
+                        <p>One of my long term goals is to become an <b>Ethical Hacker</b>. I am committed to conitnously developing the technical knowledge, discipline and expertise required to achieve that ambition.Tnank you 
+                        for taking the time to explore my work and I hope you enjoy the experience.
+                         </p>
+
+                            </div>
+                            <div id='me-text-line'>
+                            </div>
+                        </div>
+                        
                     </div>
                     <div id='skills-percent-container'>
-                        <div id='about-logo-container'>
-                            <img src="/primegotit logo green big 2.png" id='text-arrow' alt="" />
-                            <div id='logo-line'></div>
+                        <div id='about2-logo-container'>
+                            <div id='logo2-line'></div>
+
+                            <img src="/primegotit logo green big 2.png" id='text2-arrow' alt="" />
                         </div>
                         <h3>Experience levels   
                             {/* <TiArrowRightThick style={{color: 'rgb(116, 116, 116)', marginLeft: '10px', width: '35px', height: '35px'}} ></TiArrowRightThick>  */}
