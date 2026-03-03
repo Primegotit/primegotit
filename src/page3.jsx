800877e0c1bde936fb0f3d1019c3ced3cb93479c
Promise Siafwiyo <primevaldigitals@gmail.com>
2026-03-03 08:42:16 +0200

    the first push in a very long time

--- src/page3.jsx
+++ src/page3.jsx
@@ -14,9 +14,16 @@ function MyBox(Creation){
                         backgrounPosition: "center",
                         backgroundSize: "contain",
                         backgroundAttachment: "scroll",
-                        backgroundRepeat: "no-repeat"
+                        backgroundRepeat: "no-repeat",
+                        width: "100%",
+                        height: "380px",
+                        color: "white",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "spaceBetween",
+                        padding: "10px 30px",
 
-                }}>
+                    }}>
                     <div id='web-image-src-text'>
 
                     </div>
@@ -78,49 +85,62 @@ function Page3(){
             <div id='page3'>
                 <h3>Web Development</h3>
 
-                <div>
+                <div id='all-page3-container'>
+
+                    <div id='move-left-container'>
+
+                    </div>
+
+                    <div id='all-web-box-container'>
+                        {/* <MyBox 
+                                site_image = "linear-gradient(180deg,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0), black), url(kosticpic.png)"
+                                name="Kostic" 
+                                date="2024" 
+                                slogan="Built for business, designed for ease." 
+                                description="Kostic is a smart, simple POS system built to keep your business moving. 
+                                From sales to stock, it gives you tools you need without the clutter. Fast checkouts, 
+                                easy tracking and everything you need in one clean POS system."
+                                tech1 = {<li>Visual Studio</li>}
+                                tech2 = {<li>HTML</li>}
+                                tech3 = {<li>PHP</li>}
+                                tech4 = {<li>Javascript</li>}
+                                tech5 = {<li>PHP</li>}
+                                image_src = "/primegotit logo green big 2.png"                        
+                                site_url = "#"
+                                >
+                                    
+                                    
+                        </MyBox>    */}
+
+                        <MyBox 
+                                site_image = "linear-gradient(180deg,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0), black), url(trackwaypic.png)"
+                                name="Trackway" 
+                                date="2024" 
+                                slogan="Where algorithms meet the road." 
+                                description="Plan, optimize and track your deliveries in seconds. 
+                                With real time apps and intelligent route calculation, Trackway gets you from Point A to Point B the fastest way possible."
+
+                                tech1 = {<li>Visual Studio</li>}
+                                tech2 = {<li>HTML</li>}
+                                tech3 = {<li>Javascript</li>}
+                                tech4 = {<li>Vite</li>}
+                                tech5 = {<li>React</li>}
+                                image_src = "/trackway.png"                        
+                                site_url = "#"
+                                >
+                                    
+                                    
+                        </MyBox>
+                    </div>
+   
+
+
+                    <div id='move-right-container'>
+
+                    </div>
+
                     
                 </div>
-                <MyBox 
-                        site_image = "linear-gradient(180deg,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0), black), url(kosticpic.png)"
-                        name="Kostic" 
-                        date="2024" 
-                        slogan="Built for business, designed for ease." 
-                        description="Kostic is a smart, simple POS system built to keep your business moving. 
-                        From sales to stock, it gives you tools you need without the clutter. Fast checkouts, 
-                        easy tracking and everything you need in one clean POS system."
-                        tech1 = {<li>Visual Studio</li>}
-                        tech2 = {<li>HTML</li>}
-                        tech3 = {<li>Javascript</li>}
-                        tech4 = {<li>Vite</li>}
-                        tech5 = {<li>React</li>}
-                        image_src = "/primegotit logo green big 2.png"                        
-                        site_url = "#"
-                        >
-                            
-                            
-                </MyBox>   
-
-                <MyBox 
-                        site_image = "linear-gradient(180deg,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0), black), url(trackwaypic.png)"
-                        name="Trackway" 
-                        date="2024" 
-                        slogan="Built for business, designed for ease." 
-                        description="Kostic is a smart, simple POS system built to keep your business moving. 
-                        From sales to stock, it gives you tools you need without the clutter. Fast checkouts, 
-                        easy tracking and everything you need in one clean POS system."
-                        tech1 = {<li>Visual Studio</li>}
-                        tech2 = {<li>HTML</li>}
-                        tech3 = {<li>Javascript</li>}
-                        tech4 = {<li>Vite</li>}
-                        tech5 = {<li>React</li>}
-                        image_src = "/trackway.png"                        
-                        site_url = "#"
-                        >
-                            
-                            
-                </MyBox>   
-
 
 
 
