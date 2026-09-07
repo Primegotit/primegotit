aefe346ca38eb47d7471fd34c05982595c653015
Promise Siafwiyo <primevaldigitals@gmail.com>
2026-09-07 03:49:22 +0200

    asdf

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -30,12 +30,17 @@ function Resume(){
                             <Achieve_box path="/courses/aws2.png" image="/courses/aws2.png"></Achieve_box>
                             <Achieve_box path="/courses/coursera2.png" image="/courses/coursera2.png"></Achieve_box>
                             <Achieve_box path="/courses/cisco1.png" image="/courses/cisco1.png"></Achieve_box>
+                            <Achieve_box path="/courses/cisco1.png" image="/courses/google_ai.png"></Achieve_box>
+                            <Achieve_box path="/courses/cisco1.png" image="/courses/Cybersecurity.png"></Achieve_box>
+
 
                             <Achieve_box path="/courses/aws1.png" image="/courses/aws1.png" ></Achieve_box>
                             <Achieve_box path="/courses/coursera1.png" image="/courses/coursera1.png" ></Achieve_box>
                             <Achieve_box path="/courses/aws2.png" image="/courses/aws2.png"></Achieve_box>
                             <Achieve_box path="/courses/coursera2.png" image="/courses/coursera2.png"></Achieve_box>
                             <Achieve_box path="/courses/cisco1.png" image="/courses/cisco1.png"></Achieve_box>
+                            <Achieve_box path="/courses/cisco1.png" image="/courses/google_ai.png"></Achieve_box>
+                            <Achieve_box path="/courses/cisco1.png" image="/courses/Cybersecurity.png"></Achieve_box>
 
                         </div>
                         <div id='horizontal-line'>
